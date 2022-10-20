e851981f73d36d29136d7f29b7df53dcbe805db0
Ridennator <rlcpm@hotmail.com>
2022-10-20 15:42:10 +0100

    First part done

--- Fragments/app/src/main/java/com/ridennator/fragments/MainActivity.kt
+++ Fragments/app/src/main/java/com/ridennator/fragments/MainActivity.kt
@@ -4,8 +4,26 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    lateinit var f1:Fragment1
+    lateinit var f2:Fragment1
+    lateinit var f3:Fragment1
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        f1=Fragment1.newInstance("Halo","world")
+        f2=Fragment1.newInstance("Beep","Boop")
+        f3=Fragment1.newInstance("Tarn","ation")
+
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.add(R.id.fragment1,f1)
+        fragmentTransaction.add(R.id.fragment2,f2)
+        fragmentTransaction.add(R.id.fragment3,f3)
+        fragmentTransaction.addToBackStack(null)
+        fragmentTransaction.commit()
+
+
     }
 }
