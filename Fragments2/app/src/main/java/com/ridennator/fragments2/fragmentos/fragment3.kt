9708e73ea9dfa44c701d40aa9a2e569934ec9a57
Ridennator <rlcpm@hotmail.com>
2022-10-20 17:09:17 +0100

    WIP for making do something useful

--- Fragments2/app/src/main/java/com/ridennator/fragments2/fragmentos/fragment3.kt
+++ Fragments2/app/src/main/java/com/ridennator/fragments2/fragmentos/fragment3.kt
@@ -5,25 +5,50 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import com.ridennator.fragments2.R
+import kotlin.random.Random
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [fragment1.newInstance] factory method to
- * create an instance of this fragment.
- */
 class fragment3 : Fragment() {
 
     override fun onCreateView (
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?)
         :View?{
-            return inflater.inflate(R.layout.fragment_fragment1,container,false)
+            return inflater.inflate(R.layout.fragment_fragment3,container,false)
         }
+    //find button
+    var rollDicebt: Button =findViewById(R.id.GenerateNumberButton)
+    //define that the button will react to click
+    rollDicebt.setOnClickListener{
+        //add a message to screen when you press the button
+        Toast.makeText(this,"The button was pressed", Toast.LENGTH_SHORT).show()
+        rollDice()
+    }
+    diceImage = findViewById<ImageView>(R.id.DiceView)
 
+
+private fun rollDice() {
+    var randomValue = Random.nextInt(6)+1
+    var resultText = findViewById<TextView>(R.id.GeneratedNumberView)
+    resultText.text=randomValue.toString()
+
+    //choose the dice image
+    var drawableResource = when(randomValue){
+        1->R.drawable.dice_1
+        2->R.drawable.dice_2
+        3->R.drawable.dice_3
+        4->R.drawable.dice_4
+        5->R.drawable.dice_5
+        6->R.drawable.dice_6
+
+        else -> {R.drawable.empty_dice}
+    }
+    // assign image
+    //var diceImage=findViewById<ImageView>(R.id.DiceView)
+    diceImage.setImageResource(drawableResource)
+}
 }
